aa11c903d9a8d16ae3818fa60a23a4d6a8177b24
DolitsoiMykhailo <dolitsoi.mykhailo@gmail.com>
2025-08-13 15:08:38 +0300

    week1_problem6

--- C/week1/123.c
+++ C/week1/123.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include "include/cs50.h"
 
 int main(void)
 {
-    long n = get_long("awd ");
-    int m = get_int("awww ");
-    printf("%i", n / m);
+    printf("%i", 12345 % 10);
 }
 
--- C/week1/credit.c
+++ C/week1/credit.c
@@ -1,36 +1,101 @@
-#include "include/cs50.h"
+#include <cs50.h>
 #include <stdio.h>
 
-int get_input(void);
+long get_input(void);
+string is_valid(long n);
 
 int main(void)
 {
-    int credit_card = get_input();
+    long credit_card = get_input();
+    printf ("%s\n", is_valid(credit_card));
 }
 
-// get user's input
-int get_input(void)
+long get_input(void)
 {
-    long n;
-    n = get_long("Enter card number: ");
+    long n = get_long("user input: ");
     return n;
 }
 
 string is_valid(long n)
 {
-    int i = 0;
-    long cc = n;
-    while (cc > 0)
-    {
-        cc /= 10;
-        i++;
-    } 
+    int sum_odd = 0;
+    int sum_even = 0;
+    int pos = 0;
+
+    long n_temp1 = n;
+    long n_temp2 = n;
+    int length = 0;
     
-    if (i != 13 && i != 15 && i != 16)
+    // get length
+    while (n_temp1 > 0)
     {
-        printf("INVALID\n");
-        return 0;
+        n_temp1 /= 10;
+        length++;
+    }
+    // printf("%i\n", length);
+
+    // luhn algorithm
+    while (n > 0)
+    {
+        long digit = n % 10;
+        n /= 10;
+
+        // printf("%li\n", digit);
+
+        if (pos % 2 == 1)
+        {
+            if (digit >= 5)
+            {
+                sum_even += ((digit * 2) % 10) + 1;
+            }
+            else
+            {
+                sum_even += digit * 2;
+            }
+        }
+        else
+        {
+            sum_odd += digit;
+        }
+        pos++;
     }
 
-    
+    printf("%i, %i, %i\n", sum_even, sum_odd, sum_even + sum_odd);
+
+    // check if is_valid
+    if ((sum_even + sum_odd) % 10 != 0)
+    {
+        return "INVALID";
+    }
+
+    long first_two = n_temp2;
+
+    while (first_two >= 100)
+    {
+        first_two /= 10;
+    }
+
+    long first_one = first_two / 10;
+
+    printf("%i, %i, %i", first_two, first_one, length);
+
+    if (length == 15 && (first_two == 34 || first_two == 37))
+    {
+        return "AMEX";
+    }
+
+    else if (length == 16 && (first_two >= 51 && first_two <= 55))
+    {
+        return "MASTERCARD";
+    }
+
+    else if ((length == 13 || length == 16) && first_one == 4)
+    {
+        return "VISA";
+    }
+
+    else
+    {
+        return "INVALID";
+    }
 }
